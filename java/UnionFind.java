9fc1e71a096961695730859e633616ebd4b0db9d
Renliang Gu (X201) <gurenliang@gmail.com>
2016-11-20 19:18:22 -0800

    _

--- java/UnionFind.java
+++ java/UnionFind.java
@@ -1,33 +1,33 @@
 class UnionFind{
-    int[] p, size;
-    int comps;
-    UnionFind(int n){
-        p = new int[n];
-        size = new int[n];
-        reset();
+  int[] p, size;
+  int comps;
+  UnionFind(int n){
+    p = new int[n];
+    size = new int[n];
+    reset();
+  }
+  void reset(){
+    for(int i=0; i<p.length; i++){
+      p[i]=i;
+      size[i]=1;
     }
-    void reset(){
-        for(int i=0; i<p.length; i++){
-            p[i]=i;
-            size[i]=1;
-        }
-        comps = p.length;
-    }
-    int find(int x){
-        if(x!=p[x]) p[x]=find(p[x]);
-        return p[x];
-    }
-    void union(int x, int y){
-        x = find(x);
-        y = find(y);
-        if(x!=y)
-            if(size[x]>size[y]){
-                p[y]=x;
-                size[x]+=size[y];
-            }else{
-                p[x]=y;
-                size[y]+=size[x];
-            }
-        comps--;
-    }
+    comps = p.length;
+  }
+  int find(int x){
+    if(x!=p[x]) p[x]=find(p[x]);
+    return p[x];
+  }
+  void union(int x, int y){
+    x = find(x);
+    y = find(y);
+    if(x!=y)
+      if(size[x]>size[y]){
+        p[y]=x;
+        size[x]+=size[y];
+      }else{
+        p[x]=y;
+        size[y]+=size[x];
+      }
+    comps--;
+  }
 }
